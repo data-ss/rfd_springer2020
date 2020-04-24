da86150e2e8860bc456a3d84796c3a902492fddf
duryan00 <ryanngteckyan@gmail.com>
2020-04-24 11:56:28 -0400

    fixed error with file naming where the title contains '/'

--- springer.py
+++ springer.py
@@ -5,12 +5,13 @@ import requests
 txtcsv = pd.read_excel("downloadURL.xlsx")
 
 for i in txtcsv["English Package Name"].unique():
-    os.makedirs(i)
-    os.chdir(i)
-    count = txtcsv["PDF download URL"][txtcsv["English Package Name"] == i].count()
+    os.makedirs(i) # create directory
+    os.chdir(i) # change working directory into new directory
+    count = txtcsv["PDF download URL"][txtcsv["English Package Name"] == i].count() # tally up how many books in this field
     print(f"Downloading {count} books in the {i} field.")
     for j in txtcsv["PDF download URL"][txtcsv["English Package Name"] == i]:
-        title = txtcsv["Book Title"][txtcsv["PDF download URL"]==j].values[0]
+        title = txtcsv["Book Title"][txtcsv["PDF download URL"]==j].values[0].replace("/", " or ")
+        # "/" is not a valid character for naming files or folders so they need to be replaced
         print(f"Accessing book: {title}.")
         r = requests.get(j, auth=('user', 'pass'))
         open(f"{title}.pdf", 'wb').write(r.content)
