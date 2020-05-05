229bfeb87e48f20ce4f49ac9490d44a1d2add51b
duryan00 <ryanngteckyan@gmail.com>
2020-05-05 18:48:20 -0400

    Added automatic Google Drive upload via API functionality

--- README.md
+++ README.md
@@ -2,28 +2,30 @@
 Just a simple script to grab free access books from Springer for RFD, organized by their respective fields.
 
 ```
-'Engineering', 
-'Humanities, Social Sciences and Law',
-'Mathematics and Statistics', 
-'Behavioral Science',
-'Biomedical and Life Sciences', 
-'Chemistry and Materials Science',
-'Medicine', 
-'Business and Economics',
-'Earth and Environmental Science', 
-'Physics and Astronomy',
-'Computer Science', 
-'Behavioral Science and Psychology', 
-'Energy',
-'Business and Management', 
-'Religion and Philosophy',
-'Economics and Finance', 
-'Education', 
-'Law and Criminology',
-'Social Sciences', 
-'Literature, Cultural and Media Studies',
-'Intelligent Technologies and Robotics'
+1) 'Engineering', 
+2) 'Humanities, Social Sciences and Law',
+3) 'Mathematics and Statistics', 
+4) 'Behavioral Science',
+5) 'Biomedical and Life Sciences', 
+6) 'Chemistry and Materials Science',
+7) 'Medicine', 
+8) 'Business and Economics',
+9) 'Earth and Environmental Science', 
+10) 'Physics and Astronomy',
+11) 'Computer Science', 
+12) 'Behavioral Science and Psychology', 
+13) 'Energy',
+14) 'Business and Management', 
+15) 'Religion and Philosophy',
+16) 'Economics and Finance', 
+17) 'Education', 
+18) 'Law and Criminology',
+19) 'Social Sciences', 
+20) 'Literature, Cultural and Media Studies',
+21) 'Intelligent Technologies and Robotics'
 ```
+
+Automatic upload to Google Drive is built-in. Download your client_secrets.json file and place it on the same level as this notebook before proceeding to the next cell. You will then be prompted to authorize the app.
 
 Special thanks to **marcusdan18** for sharing this deal, and **MichaelSun** for compiling the direct download links.
 
--- springer.py
+++ springer.py
@@ -1,23 +1,48 @@
 import pandas as pd
-import os
 import requests
+import os
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from tqdm import tqdm
+
+### Functions and calls to Google Pydrive to automatically upload to Google Drive
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication.
+drive = GoogleDrive(gauth)
+
+def createDriveFolder(folderName):
+    folder_metadata = {'title' : folderName, 'mimeType' : 'application/vnd.google-apps.folder'}
+    folder = drive.CreateFile(folder_metadata)
+    folder.Upload()
+    foldertitle = folder['title']
+    folderid = folder['id']
+    print('title: %s, id: %s' % (foldertitle, folderid))
+    return folderid
+
+def uploadDriveFile(folderid, file_path):
+    file = drive.CreateFile({"parents": [{"kind": "drive#fileLink", "id": folderid}], 'title': os.path.basename(os.path.abspath(file_path))})
+    file.SetContentFile(os.path.abspath(file_path))
+    file.Upload()
+###
 
 txtcsv = pd.read_excel("downloadURL.xlsx")
 
-for i in txtcsv["English Package Name"].unique():
+for i in tqdm(txtcsv["English Package Name"].unique()):
     os.makedirs(i) # create directory
     os.chdir(i) # change working directory into new directory
+    drive_id = createDriveFolder(i)
     count = txtcsv["PDF download URL"][txtcsv["English Package Name"] == i].count() # tally up how many books in this field
-    print(f"Downloading {count} books in the {i} field.")
-    for j in txtcsv["PDF download URL"][txtcsv["English Package Name"] == i]:
+    print(f"\nDownloading {count} books in the {i} field.\n")
+    for j in tqdm(txtcsv["PDF download URL"][txtcsv["English Package Name"] == i]):
         title = txtcsv["Book Title"][txtcsv["PDF download URL"]==j].values[0].replace("/", " or ")
         # "/" is not a valid character for naming files or folders so they need to be replaced
-        print(f"Accessing book: {title}.")
+        print(f"\nAccessing book: {title}.\n")
         r = requests.get(j, auth=('user', 'pass'))
-        open(f"{title}.pdf", 'wb').write(r.content)
+        open(f"\n{title}.pdf", 'wb').write(r.content)
         if os.path.isfile(f"{title}.pdf"):
-            print(f"{title} is now in the {i} folder.")
+            print(f"\n{title} is now in the {i} folder.\n")
         else:
-            print(f"{title} failed to download.")
+            print(f"\n{title} failed to download.\n")
+        uploadDriveFile(drive_id, f"{title}.pdf")
     os.chdir("..")
-    print(f"All books in the {i} category have been downloaded.")
+    print(f"\nAll books in the {i} category have been downloaded.\n")
